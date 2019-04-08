e3358b7a1e9677d53e74ef4ca17e0d7b74639c63
Sergey <pmswga@gmail.com>
2019-04-08 18:21:15 +0300

    include semantic & create test view

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -3,6 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- jQuery -->
+        <script src="{{ asset('js/jquery.js') }}" ></script>
+
+        <!-- Semantic UI -->
+        <link href="{{ asset('css/semantic/semantic.css') }}" rel="stylesheet">
+        <script src="{{ asset('css/semantic/semantic.js') }}" ></script>
 
         <title>Laravel</title>
 
@@ -64,36 +71,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="ui menu">
+            <div class="header item">Student Helper</div>
         </div>
     </body>
 </html>
