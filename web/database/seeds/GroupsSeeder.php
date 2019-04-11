348b33d8603702650a1ca2a758056fe8697e8bdd
Sergey <pmswga@gmail.com>
2019-04-11 20:56:19 +0300

    add groups seeder & and update students seeder

---
+++ web/database/seeds/GroupsSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GroupsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table()->insert([
+            [
+                'name' => 'ИКБО-07-18'
+            ],
+            [
+                'name' => 'ИКБО-08-18'
+            ],
+            [
+                'name' => 'ИКБО-09-18'
+            ],
+            [
+                'name' => 'ИКБО-10-18'
+            ],
+            [
+                'name' => 'ИКБО-11-18'
+            ],
+            [
+                'name' => 'ИКБО-12-18'
+            ],
+            [
+                'name' => 'ИКБО-13-18'
+            ],
+            [
+                'name' => 'ИКБО-14-18'
+            ],
+        ]);
+    }
+}
--- web/database/seeds/StudentsSeeder.php
+++ web/database/seeds/StudentsSeeder.php
@@ -16,7 +16,61 @@ class StudentsSeeder extends Seeder
                 'sn' => 'Басыров',
                 'fn' => 'Сергей',
                 'pt' => 'Амирович',
-                'group_id' => 1,
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Батов',
+                'fn' => 'Иван',
+                'pt' => 'Николаевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Дорожков',
+                'fn' => 'Кирилл',
+                'pt' => 'Дмитриевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Дворников',
+                'fn' => 'Дмитрий',
+                'pt' => 'Валерьевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Дубровский',
+                'fn' => 'Дмитрий',
+                'pt' => 'Алексеевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Запертов',
+                'fn' => 'Иван',
+                'pt' => 'Дмитриеивч',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Зейналов',
+                'fn' => 'Магеррам',
+                'pt' => 'Гилал Оглы',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Зыков',
+                'fn' => 'Александр',
+                'pt' => 'Алексеевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Квасов',
+                'fn' => 'Алексей',
+                'pt' => 'Владиславович',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-07-18')->get()->first()->id,
+            ],
+            [
+                'sn' => 'Набиев',
+                'fn' => 'Анушервон',
+                'pt' => 'Сухробшоевич',
+                'group_id' => DB::table("Groups")->where('name', 'ИКБО-12-18')->get()->first()->id,
             ],
         ]);
     }
